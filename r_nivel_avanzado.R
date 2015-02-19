906c17c66922e79e20e638a9aab2837430bbc423
Diego Huaraca <diego.21.phs@hotmail.com>
2015-02-18 20:17:32 -0500

    Update

--- r_nivel_avanzado.R
+++ r_nivel_avanzado.R
@@ -77,6 +77,59 @@ sqldf("SELECT m01.PRODUCTO, m01.SALDO - m02.SALDO AS DIFF
       from m01 INNER JOIN m02 ON m01.IDENTIFICACION = m02.IDENTIFICACION
       GROUP BY m01.PRODUCTO")
 
+Con respeCto al mes de febrero se requiere la siguiente información:
+¿Cuántos clientes de segmento "CORPORATIVO" tienen "TARJETA DE CREDITO" con saldos mayores a 50 dólares?
+
+sqldf("SELECT COUNT(*) AS SUJETOS
+      from m02 
+      WHERE SEGMENTO LIKE 'CORPORATIVO' AND PRODUCTO LIKE 'TARJETA DE CREDITO' AND SALDO > 50")
+
+¿Cuál es la suma en saldo de los clientes gestionados en las oficinas "NORTE" y "CENTRO" que tienen productos de "CUENTA AHORROS" y "CUENTA CORRIENTE"?.
+
+sqldf("SELECT SUM(SALDO) AS SALDOS
+      from m02 
+      WHERE OFICINA IN ('NORTE', 'CENTRO') AND PRODUCTO IN ('CUENTA AHORROS', 'CUENTA CORRIENTE')")
+
+¿Cuántas "MUJERES" tiene "CERTIFICADOS A PLAZO" con montos entre 10 y 30 dólares?.
+
+sqldf("SELECT COUNT(*) MUJERES
+      from m02 
+      WHERE GENERO LIKE 'MUJER' AND PRODUCTO LIKE 'CERTIFICADO A PLAZO' AND SALDO BETWEEN 10 AND 30 ")
+
+¿Cuál es el promedio de saldos en la oficina "CENTRO" en "TARJETAS DE DEBITO"?.
+
+sqldf("SELECT SUM(SALDO)/COUNT(*) AS SALDO_PROMEDIO
+      from m02 
+      WHERE OFICINA LIKE 'CENTRO' AND PRODUCTO LIKE 'TARJETA DE CREDITO' ")
+
+### Semana 2
+# duplicamos la información
+file.copy("enero.txt","marzo.txt")
+file.copy("enero.txt","abril.txt")
+file.copy("febrero.txt","mayo.txt")
+file.copy("febrero.txt","junio.txt")
+
+file.rename("enero.txt", "ind_enero.txt")
+file.rename("febrero.txt", "ind_febrero.txt")
+file.rename("marzo.txt", "ind_marzo.txt")
+file.rename("abril.txt", "dat_abril.txt")
+file.rename("mayo.txt", "dat_mayo.txt")
+file.rename("junio.txt", "dat_junio.txt")
+
+
+mes <- c('ind_enero.txt', 'ind_febrero.txt', 'ind_marzo.txt', 'dat_abril.txt', 'dat_mayo.txt', 'dat_junio.txt')
+f00 <- data.frame('ARCHIVO'=as.character(mes), stringsAsFactors = FALSE)
+f00$ORDEN <- rownames(f00)
+
+library(data.table)
+data <- c()
+for(i in 1:nrow(f00)){
+    file <- cbind(fread(f00[i,1]), "ARCHIVO"=i)
+    data <- rbind(data, file)
+}
+
+by(data, data$ARCHIVO, nrow)
+
 
 
 
