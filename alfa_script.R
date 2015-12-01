cd64fc6fdcdcd35458108d2a4b8c9e9c2dfcb027
duhi23 <diego.21.phs@hotmail.com>
2015-12-01 09:07:34 -0500

    Update Script

--- alfa_script.R
+++ alfa_script.R
@@ -2,23 +2,17 @@
 #####     Algoritmo de seleccion     #####
 ##########################################
 
-install.packages('aod', dependencies = TRUE)
-ls("package:aod")
-library(dplyr)
-library(aod)
-
 alfa <- function(var, datos){
-    library(dplyr)
+    suppressMessages(library(dplyr))
     pos <- which(colnames(datos)!=var)
     var <- which(colnames(datos)==var)
     tab <- c()
     res <- sapply(pos, function(i){
         glm(datos[[var]] ~ datos[[i]], data=datos, family="binomial")$aic})
-    tab <- tbl_df(data.frame(variable=colnames(datos)[pos], aic=res)) %>% arrange(desc(aic))
+    tab <- tbl_df(data.frame(variable=colnames(datos)[pos], aic=res)) %>% arrange(aic)
     return(tab)
 }
 
-alfa("vs", mydata)
 
 library(foreign)
 library(dplyr)
@@ -27,6 +21,8 @@ ls("package:foreign")
 info <- tbl_df(read.spss("data_modelo.sav", to.data.frame = TRUE))
 info <- info %>% filter(VAR_DEP %in% c("BUENO", "MALO"))
 info <- info %>% mutate(VAR=ifelse(VAR_DEP=="BUENO",0,1))
+
+sapply(info, function(x) sum(is.na(x)))
 
 vec <- as.vector(which(unlist(lapply(info, class))=="numeric"))
 
@@ -40,3 +36,4 @@ vec3 <- vec2[!is.na(vec2)]
 info1 <- info %>% select(vec3)
 
 resul <- alfa("VAR", info1)
+resul
